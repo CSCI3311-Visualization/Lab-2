03cb179df1876a53f7b149f021c16c2eb7e46a17
mylee1995 <mylee1995@gmail.com>
2020-09-10 22:50:53 -0400

    script logic complete

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // TODO: load the dataset
+let attractions;
+
+fetch('./attractions.json')
+  .then((response) => response.json())
+  .then((data) => {
+    attractions = data;
+  })
+  .then(() => {
+    filterData('all');
+  });
 
 function filterData(category) {
   /* **************************************************
@@ -14,7 +24,24 @@ function filterData(category) {
    * - the max. length of 'data' is 5
    *
    * **************************************************/
+  let filtered = attractions;
+  if (category != 'all') {
+    filtered = attractions.filter(
+      (attraction) => attraction.Category == category
+    );
+  }
+  filtered.sort((a, b) => b.Visitors - a.Visitors);
+  let rendered = filtered.filter((v, i) => i < 5);
+  console.log('rendered', rendered);
+  renderBarChart(rendered);
+}
+
+function handler(event) {
+  console.log('event target value', event.target.value);
+  filterData(event.target.value);
 }
 
 // TODO: Define an event listener for the dropdown menu
 //       Call filterData with the selected category
+document.querySelector('#attraction-category');
+document.addEventListener('change', handler);
